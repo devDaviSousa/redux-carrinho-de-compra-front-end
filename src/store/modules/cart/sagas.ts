544dde504f30ea6dbf1ef0df690046527cdacd4c
devDaviSousa <devsousadavi@gmail.com>
2022-03-21 21:16:17 -0300

    obtendo quantidade do carrinho

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,17 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, select, takeLatest } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductToCar } from "./actions";
 
-function checkProductStock() {
+type CheckProductStockRequest = ReturnType<typeof addProductToCar>;
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+  console.log(currentQuantity);
   console.log("Adicionou o carrinho");
 }
 
